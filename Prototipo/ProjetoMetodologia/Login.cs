4af8d64ef9173b418a5abfadfab613fa7006a1a4
ekxilled <arisobex@gmail.com>
2016-06-01 20:47:27 +0100

    Log In

--- Prototipo/ProjetoMetodologia/Login.cs
+++ Prototipo/ProjetoMetodologia/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ProjetoMetodologia
 {
@@ -24,12 +25,21 @@ namespace ProjetoMetodologia
 
         private void btnenter_Click(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\ekxilled\Documents\Data.mdf; Integrated Security = True; Connect Timeout = 30;");
+            SqlDataAdapter sda = new SqlDataAdapter("Select count(*) from [Table] where Username = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
 
-            //opens homepage, corre quando o login for true
-            Homepage homepage = new Homepage();
+                //opens homepage, corre quando o login for true
+                Homepage homepage = new Homepage();
 
-            if (homepage.ShowDialog() != DialogResult.OK)
-                return;
+                if (homepage.ShowDialog() != DialogResult.OK)
+                    return;
+            }
+            else
+            { MessageBox.Show("Please check your Credentials"); }
         }
     }
 }
--- Prototipo/ProjetoMetodologia/LoginDB.Designer.cs
+++ Prototipo/ProjetoMetodologia/LoginDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\ryan\documents\visual studio 2015\Projects\ProjetoMetodologia\ProjetoMetodologia\LoginDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ekxilled\Documents\GitHub\TESP_Grupo6_MDS\Prototipo\ProjetoMetodologia\LoginDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
