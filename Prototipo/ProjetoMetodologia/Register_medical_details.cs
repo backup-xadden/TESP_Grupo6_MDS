2c70205e91a89ea001eff1aa6a1acdda3bcc4f1a
RyanCarvalho <ryanpedrocarvalho@hotmail.com>
2016-06-01 18:02:27 +0100

    KEK

---
+++ Prototipo/ProjetoMetodologia/Register_medical_details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoMetodologia
+{
+    public partial class Register_medical_details : Form
+    {
+        public Register_medical_details()
+        {
+            InitializeComponent();
+        }
+    }
+}
